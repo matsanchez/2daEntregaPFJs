77fc3dc772f1e78d1f4a2ab6478a509766b5954d
matsanchez <smatu22@gmail.com>
2022-06-27 14:11:40 -0300

    Subiendo 2da entrega

---
+++ js/cartMain.js
@@ -0,0 +1,120 @@
+let merchanProducts = [
+    {
+        id: 01,
+        name:"Bolsa Ecologica",
+        price:50,
+        img:"../img/merchandising/bolsaeco.png"
+    },
+    {
+        id: 02,
+        name:"Cuaderno",
+        price:500,
+        img:"./img/merchandising/cuaderno.png"
+    },
+    {
+        id: 03,
+        name:"Gorra",
+        price:800,
+        img:"./img/merchandising/gorra.png"
+    },
+    {
+        id: 04,
+        name:"Mate",
+        price:500,
+        img:"./img/merchandising/mate.png"
+    },
+    {
+        id: 05,
+        name:"Matera",
+        price:1500,
+        img:"./img/merchandising/matera.png"
+    },
+    {
+        id: 06,
+        name:"Mochila",
+        price:2500,
+        img:"./img/merchandising/mochila.png"
+    },
+    {
+        id: 07,
+        name:"Porta Notebook",
+        price:1800,
+        img:"./img/merchandising/portanotebook.png",
+    
+    },
+    {
+        id: 08,
+        name:"Remera",
+        price:3000,
+        img:"./img/merchandising/remera.png"
+    },
+    {
+        id: 09,
+        name:"Termo",
+        price:6000,
+        img:"./img/merchandising/termo.png"
+    },
+    {
+        id: 10,
+        name:"Articulos de Oficina",
+        price:1500,
+        img:"./img/merchandising/varios.png"
+    }
+]
+let users;
+let cart;
+
+if(JSON.parse(localStorage.getItem('cart')) || JSON.parse(localStorage.getItem('users')))  {
+    cart = JSON.parse(localStorage.getItem('cart'));
+    users = JSON.parse(localStorage.getItem('users'))
+} else {
+    localStorage.setItem('cart', JSON.stringify([]))
+    localStorage.setItem('users', JSON.stringify([]))
+    cart = JSON.parse(localStorage.getItem('cart'))
+    users = JSON.parse(localStorage.getItem('users'))
+}
+
+function showProducts (){
+    for (let i = 0; i < merchanProducts.length; i++) {
+        const element = merchanProducts[i];
+        const { id, name, price, img } = element
+            const card = `
+                <div class="card text-center m-2" style="width: 18rem;">
+                    <img src=${img} class="d-block imgProducts img-fluid" alt="${name}">
+                    <div class="card-body">
+                    <h5 class="card-title">${name}</h5>
+                    <p class="card-price">$${price}</p>
+                    <button id=${id} class="btn btn-warning">Agregar al Carrito</button>
+                    </div>
+                </div>
+                `
+            const containerProduct = document.getElementById('containerProduct')
+            containerProduct.innerHTML += card
+    }
+}
+showProducts();
+
+const addBtn = document.getElementsByClassName('btn');
+
+for (let i = 0; i < addBtn.length; i++) {
+    const element = addBtn[i];
+    element.addEventListener('click', addCart)
+}
+
+function addCart(e){
+    const btn = e.target;
+    const idBoton = btn.getAttribute('id')
+    const idFound = merchanProducts.find(prod => prod.id == idBoton)
+    const inCart = cart.find(prod => prod.id == idFound.id)
+    if(!inCart) {
+        cart.push({...idFound, cantidad: 1})
+    } else {
+        let cartFilter = cart.filter(id => id.id != inCart.id)
+        cart = [...cartFilter, {...inCart, cantidad: inCart.cantidad + 1}]
+    }
+    localStorage.setItem('cart', JSON.stringify(cart))
+    location.reload();
+}
+
+const counter = document.getElementById('cartCounter');
+counter.innerHTML = cart.length;
---
+++ js/main.js
@@ -0,0 +1,63 @@
+let users=[];
+
+if(JSON.parse(localStorage.getItem('users')))  {
+    users = JSON.parse(localStorage.getItem('users'))
+} else {
+    localStorage.setItem('users', JSON.stringify([]))
+    users = JSON.parse(localStorage.getItem('users'))
+}
+
+const DOMlogin = document.getElementById('domLogin');
+DOMlogin.innerHTML = `
+        <div class="row align-items-stretch">
+            <div class="col bgHome_left d-none d-lg-block col-md-5 col-lg-5 col-xl-6 rounded"></div>
+                <div class="col bgHome_right p-5 rounded-end">
+                    <div class="text-end">
+                        <img src="./img/logomenu.png" width="300" alt=""> 
+                    </div>
+                <h2 class="fw-bold text-center py-5">BIENVENIDO</h2>
+                <form>
+                    <div class="mb-4">
+                        <label for="user" class="form-label">Ingresa tu nombre</label>
+                        <input type="user" class="form-control" name="user" id="name">
+                    </div>
+                    <div class="mb-4">
+                        <label for="email" class="form-label">Correo Electronico</label>
+                        <input type="email" class="form-control" name="email" id="email">
+                    </div>
+                </form>
+                <div class="d-grid">
+                    <button type="button" id="getInfo" class="btn btn-info">Iniciar Sesion</button>
+                </div>
+            </div>
+        </div>
+`;
+
+const DOMgetInfo = document.getElementById('getInfo');
+DOMgetInfo.addEventListener('click',setUser)
+
+class GetUser {
+  constructor(name, email) {
+    this.name = name;
+    this.email = email;
+  }
+
+}
+
+function setUser() {
+
+  let name = document.getElementById('name').value;
+
+  let email = document.getElementById('email').value
+
+  let user = new GetUser(name, email);
+
+  users.push(user);
+
+  localStorage.setItem('users', JSON.stringify(users));
+
+  window.location.replace("index.html");
+}
+
+
+
